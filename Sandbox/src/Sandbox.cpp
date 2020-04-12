33c62fcfca96255414345d2f19c62b099770a445
Bruce Cosgrove <1901.brc@gmail.com>
2020-04-11 23:25:33 -0700

    Added Premake

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -14,6 +14,12 @@ public:
 	}
 };
 
+/*
+ * Trello: https://trello.com/b/DaO3KVu2/gbc-12
+ * GitHub: https://github.com/BruceTheShark42/GBC-1.2
+ * Cherno: https://www.youtube.com/watch?v=xnopUoZbMEk&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=9
+ */
+
 gbc::Application* gbc::createApplication()
 {
 	return new Sandbox();
---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "GBC 1.2"
+	architecture "x64"
+	startproject "Sandbox"
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GBC"
+	location "GBC"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+			"GBC_PLATFORM_WINDOWS",
+			"GBC_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GBC_DEBUG"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "GBC_RELEASE"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "GBC_DIST"
+		optimize "on"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"GBC/vendor/spdlog/include",
+		"GBC/src"
+	}
+
+	links {
+		"GBC"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+			"GBC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GBC_DEBUG"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "GBC_RELEASE"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "GBC_DIST"
+		optimize "on"
